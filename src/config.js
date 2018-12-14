bb653d90bf873da59933767115e83a725e7f2813
awesomearpit <jarpit1298@gmail.com>
2018-12-15 00:28:40 +0530

    issues resolved

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-const URL =
-  "http://api.bitly.com/v3/shorten?login=o_2rbg6sd5jn&apikey=R_0bd2e6a02eeb4b20a79de9535d0be4b0&longUrl=";
+const URL = `http://api.bitly.com/v3/shorten?login=o_2rbg6sd5jn&apikey=${
+  process.env.APIKEY
+}&longUrl=`;
 
 export { URL };
